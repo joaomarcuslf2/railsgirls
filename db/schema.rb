58938d37af9e0707e1045cf4c2e65751b1505a62
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-06-07 01:11:14 -0300

    Added comments scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601111845) do
+ActiveRecord::Schema.define(version: 20170607030814) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
